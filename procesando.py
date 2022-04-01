a2ecef9e0243f5f50b3e5ac0cbad26ae77a4f9f5
sheyla ydalia <90335656+YdaliaS@users.noreply.github.com>
2022-03-31 21:05:15 -0500

    Add files via upload

---
+++ procesando.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+class Procesando():
+
+    def mover(self):
+        os.system("cls")
+
+        espacios = " "
+        for i in range(1):
+
+            print("{} PROCESANDO BOLETA DE PAGO...".format(espacios))
+
+            espacios  += " "
+
+            time.sleep(0.3)
+            os.system("cls")
+
+            print("{}  PROCESANDO BOLETA DE PAGO...".format(espacios))
+
+            espacios  += " "
+
+            time.sleep(0.3)
+            os.system("cls")
+
+            print("{}  PROCESANDO BOLETA DE PAGO...".format(espacios))
+
+            espacios  += " "
+
+            time.sleep(0.3)
+            os.system("cls")
+
+            print("{}  PROCESANDO BOLETA DE PAGO...".format(espacios))
+
+            espacios  += " "
+
+            time.sleep(0.3)
+            os.system("cls")
