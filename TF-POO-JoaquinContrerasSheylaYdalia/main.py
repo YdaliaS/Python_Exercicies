d064f1b12f6fa65add1e6f5ce944e4d821e0f2e1
sheyla ydalia <90335656+YdaliaS@users.noreply.github.com>
2022-03-31 21:04:34 -0500

    Add files via upload

---
+++ TF-POO-JoaquinContrerasSheylaYdalia/main.py
@@ -0,0 +1,41 @@
+from boleta import Boleta
+from procesando import Procesando
+
+Trabajador1 = Boleta()
+Proceso = Procesando()
+print("__________________________________")
+print("      ______________________      ")
+print("     |                      |     ")
+print("     |    FERROTEK  S.A.C   |     ")
+print("     |______________________|     ")
+print("                                  ")
+print("      *** DATOS DE ENTRADA ***    ")
+print("                                  ")
+Trabajador1.ObtenerDatos()
+print("                                  ")
+print("__________________________________")
+print("\n")
+if Trabajador1.Sueldo_Basico() > 0:
+    Proceso.mover()
+
+    print("__________________________________")
+    print("      ______________________      ")
+    print("     |                      |     ")
+    print("     |    FERROTEK  S.A.C   |     ")
+    print("     |______________________|     ")
+    print("                                  ")
+    print("      *** BOLETA DE PAGO ***      ")
+    print("                                  ")
+    print("    NOMBRE:              ", Trabajador1.trabajador.title())
+    print("    CATEGORIA:           ", Trabajador1.categoria.upper())
+    print("    SUELDO BASICO:       ", Trabajador1.Sueldo_Basico())
+    print("    DESCUENTO TARDANZAS: ", round(Trabajador1.Descuento_Tardanzas(), 2))
+    print("    PAGO HORAS EXTRAS:   ", round(Trabajador1.Pago_Extras(), 2))
+    print("    SUELDO NETO:         ", round(Trabajador1.Sueldo_Neto(), 2))
+    print("                                   ")
+    print("___________________________________")
+
+    input("PRESIONE ENTER PARA SALIR...")
+    print("\nGracias por su preferencia, regrese pronto")
+else:
+    print("CATEGORIA NO ENCONTRADA!!!")
