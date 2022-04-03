d2c2ec6e46f6b45e3170fe49e7e3744a243eb271
sheyla ydalia <90335656+YdaliaS@users.noreply.github.com>
2022-04-02 21:08:18 -0500

    Update main.my

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from trabajador import Trabajador
 from boleta import Boleta
-from procesando import Procesando
+import os
+import time
 
-Trabajador1 = Boleta()
-Proceso = Procesando()
-print("__________________________________")
-print("      ______________________      ")
-print("     |                      |     ")
-print("     |    FERROTEK  S.A.C   |     ")
-print("     |______________________|     ")
-print("                                  ")
-print("      *** DATOS DE ENTRADA ***    ")
-print("                                  ")
-Trabajador1.ObtenerDatos()
-print("                                  ")
-print("__________________________________")
-print("\n")
-if Trabajador1.Sueldo_Basico() > 0:
-    Proceso.mover()
+Boleta1 = Boleta()
+Trabajador1 = Trabajador()
+os.system("cls")
 
+Trabajador1.ObtenerDatosT();Boleta1.ObtenerDatosB()
+
+if Boleta1.Sueldo_Basico() > 0:
+
+    espacios = " "
+    for i in range(5):
+        print(" PROCESANDO BOLETA DE PAGO...".format(espacios))
+        espacios+= " "; time.sleep(0.3); os.system("cls")
+
+    Trabajador1.Imprimir()
+    print("    CATEGORIA:           ",Boleta1.categoria)
+    print("    HORAS EXTRAS:        ",Boleta1.horas_extras)
+    print("    TARDANZAS:(minutos)  ",Boleta1.tardanzas)
     print("__________________________________")
     print("      ______________________      ")
     print("     |                      |     ")
@@ -27,11 +29,11 @@ if Trabajador1.Sueldo_Basico() > 0:
     print("      *** BOLETA DE PAGO ***      ")
     print("                                  ")
     print("    NOMBRE:              ", Trabajador1.trabajador.title())
-    print("    CATEGORIA:           ", Trabajador1.categoria.upper())
-    print("    SUELDO BASICO:       ", Trabajador1.Sueldo_Basico())
-    print("    DESCUENTO TARDANZAS: ", round(Trabajador1.Descuento_Tardanzas(), 2))
-    print("    PAGO HORAS EXTRAS:   ", round(Trabajador1.Pago_Extras(), 2))
-    print("    SUELDO NETO:         ", round(Trabajador1.Sueldo_Neto(), 2))
+    print("    CATEGORIA:           ", Boleta1.categoria.upper())
+    print("    SUELDO BASICO:       ", Boleta1.Sueldo_Basico())
+    print("    DESCUENTO TARDANZAS: ", round(Boleta1.Descuento_Tardanzas(), 2))
+    print("    PAGO HORAS EXTRAS:   ", round(Boleta1.Pago_Extras(), 2))
+    print("    SUELDO NETO:         ", round(Boleta1.Sueldo_Neto(), 2))
     print("                                   ")
     print("___________________________________")
 
@@ -39,3 +41,4 @@ if Trabajador1.Sueldo_Basico() > 0:
     print("\nGracias por su preferencia, regrese pronto")
 else:
     print("CATEGORIA NO ENCONTRADA!!!")
+    input("PRESIONE ENTER PARA SALIR...")
