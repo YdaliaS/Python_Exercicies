6a67e35b6be8c2c949c387270ff17ca07dd9347b
sheyla ydalia <90335656+YdaliaS@users.noreply.github.com>
2022-04-02 21:02:51 -0500

    Update boleta.py

--- boleta.py
+++ boleta.py
@@ -1,30 +1,26 @@
-from trabajador import Trabajador
+class Boleta(): # Clase "Boleta"
+    def __init__(self):
+        self.categoria = None
+        self.horas_extras = None
+        self.tardanzas = None
 
-# Clase Hijo
-class Boleta(Trabajador): # Clase "Boleta"
-    # Metodos
+    def ObtenerDatosB(self): # Metodo "ObtenerDatos"
+        self.categoria = input("CATEGORIA:                 ")
+        self.horas_extras = float(input("HORAS EXTRAS:              "))
+        self.tardanzas = float(input("TARDANZAS: (minutos)       "))
+
     def Sueldo_Basico(self): # Metodo "Sueldo_basico"
-        if self.categoria == "A" or self.categoria == "a":
-            sb = 3000
-            return sb
-        elif  self.categoria =="B" or self.categoria == "b":
-            sb = 2500
-            return sb
-        elif self.categoria == "C" or self.categoria == "c":
-            sb = 2000
-            return sb
-        else:
-            sb = 0
-            return sb
+        self.categoria = self.categoria.upper()
+        if self.categoria == "A": sb = 3000; return sb
+        elif self.categoria =="B": sb = 2500; return sb
+        elif self.categoria == "C": sb = 2000; return sb
+        else: sb = 0; return sb
 
     def Descuento_Tardanzas(self): # Metodo "Descuento_tardanzas"
-        dsc = ((self.Sueldo_Basico()/240)/60)*self.tardanzas
-        return dsc
+        dsc = ((self.Sueldo_Basico()/240)/60)*self.tardanzas; return dsc
 
     def Pago_Extras(self): # Metodo "Pago_extras"
-            comision = (self.Sueldo_Basico()/240)*self.horas_extras
-            return comision
+            comision = (self.Sueldo_Basico()/240)*self.horas_extras; return comision
 
     def Sueldo_Neto(self): # Metodo "Sueldo_neto"
-        total =(self.Sueldo_Basico()+self.Pago_Extras()-self.Descuento_Tardanzas())
-        return total
+        total =(self.Sueldo_Basico()+self.Pago_Extras()-self.Descuento_Tardanzas()); return total
